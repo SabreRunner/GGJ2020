ec882883f7e9c36f61f7119af6b35721dde3b62f
SabreRunner <%Z*!3Mg!6kSUbE5EjRXMum4dNHq8s#yc>
2020-01-31 17:51:11 +0200

    Created Specific Instantiator. Added fire risk.

--- Assets/GGJ2020/ContinentBlockController.cs
+++ Assets/GGJ2020/ContinentBlockController.cs
@@ -1,8 +1,14 @@
-﻿using GGJ2020.Events;
-using UnityEngine;
-
-namespace GGJ2020
+﻿namespace GGJ2020
 {
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using PushForward;
+    using PushForward.ExtensionMethods;
+    using Random = UnityEngine.Random;
+    using Events;
+    using UnityEngine;
+
     public class ContinentBlockController : BaseMonoBehaviour
     {
         public enum ResourceType { None, Trees, Water }
@@ -12,6 +18,27 @@ namespace GGJ2020
         public ResourceType Resource => this.resource;
         [SerializeField] private StatusType status;
         public StatusType Status => this.status;
+        public float fireRisk;
+        [SerializeField] private SpecificInstantiator[] objectInstantiators;
+        private List<SpecificInstantiator> TreeInstantiators
+        {
+            get
+            {
+                return new List<SpecificInstantiator>(this.objectInstantiators)
+                    .FindAll(instantiator => instantiator.spawnType == GameConfiguration.SpawnType.None
+                                             || instantiator.spawnType == GameConfiguration.SpawnType.Fire);
+            }
+        }
+
+        private List<SpecificInstantiator> FireInstantiators
+        {
+            get
+            {
+                return new List<SpecificInstantiator>(this.objectInstantiators)
+                    .FindAll(instantiator => instantiator.spawnType == GameConfiguration.SpawnType.None
+                                             || instantiator.spawnType == GameConfiguration.SpawnType.Tree);
+            }
+        }
 
         public void ResourceGrabbed()
         {
@@ -25,6 +52,43 @@ namespace GGJ2020
             { return; }
 
 
+        }
+
+        public void CreateRandomFire()
+        {
+            List<SpecificInstantiator> fireInstantiators = this.FireInstantiators;
+            fireInstantiators[Random.Range(0, fireInstantiators.Count - 1)].Instantiate(GameConfiguration.SpawnType.Fire);
+        }
+        
+        private WaitForSeconds fireWaitForSeconds;
+        private IEnumerator FireCoroutine()
+        {
+            while (this.FireInstantiators.Count > 0)
+            {
+                float roll = Random.Range(0f, 1f);
+                // this.Temp("FireCoroutine", "Rolled: " + roll);
+                if (roll.Between(0, this.fireRisk))
+                { this.CreateRandomFire(); }
+                yield return this.fireWaitForSeconds;
+                this.fireRisk += GameManager.Instance.gameConfiguration.fireRiskIncrease;
+            }
+        }
+        
+        private void StartFirePotential()
+        {
+            this.fireRisk = GameManager.Instance.gameConfiguration.initialFireRisk;
+            this.StartCoroutine(this.FireCoroutine());
+        }
+
+        private void Start()
+        {
+            this.fireWaitForSeconds = new WaitForSeconds(GameManager.Instance.gameConfiguration.fireRiskTimeStepInSeconds);
+            this.StartFirePotential();
+        }
+
+        private void OnValidate()
+        {
+            this.objectInstantiators = this.GetComponentsInChildren<SpecificInstantiator>();
         }
     }
 }
