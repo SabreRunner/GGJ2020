e12ef684b18d950aef17229796ad4787aedf622c
Kfir Nissim <kfirr99@gmail.com>
2020-01-31 21:36:10 +0200

    add pause and play button

---
+++ Assets/GGJ2020/Prefabs/UI/PauseAndPlay.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GGJ2020
+{
+    public class PauseAndPlay : BaseMonoBehaviour
+    {
+        private Image image;
+        private bool IsPlaying = true;
+
+        public Sprite PauseImage;
+        public Sprite PlayImage;
+
+        public void Toggle()
+        {
+            IsPlaying = !IsPlaying;
+            image.sprite = IsPlaying ? PauseImage : PlayImage;
+        }
+
+        // Update is called once per frame
+        void Start()
+        {
+            image = GetComponent<Image>();
+            image.sprite = IsPlaying ? PauseImage : PlayImage;
+        }
+    }
+}
