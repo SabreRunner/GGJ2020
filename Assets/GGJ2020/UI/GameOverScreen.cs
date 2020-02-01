be07f725f220119aa1940f23b9bb483ccca34745
Kfir Nissim <kfirr99@gmail.com>
2020-02-01 16:52:04 +0200

    add game over screen

---
+++ Assets/GGJ2020/UI/GameOverScreen.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GGJ2020
+{
+    public class GameOverScreen : MonoBehaviour
+    {
+        public Sprite WinImage;
+        public Sprite LoseImage;
+
+        private Image screenImage;
+
+        public void SetScreen(bool vicroty)
+        {
+            this.screenImage.sprite = vicroty ? this.WinImage : this.LoseImage;
+        }
+
+        void Start()
+        {
+            this.screenImage = this.GetComponent<Image>();
+        }
+    }
+}
