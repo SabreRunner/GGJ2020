982c672f18bf3cf2564cd869247771e9bf749d9b
Kfir Nissim <kfirr99@gmail.com>
2020-02-01 16:28:28 +0200

    add end screen

---
+++ Assets/GGJ2020/UI/EndScreen.cs
@@ -0,0 +1,12 @@
+﻿namespace GGJ2020.UI
+{
+    using UnityEngine;
+
+    public class EndScreen : MonoBehaviour
+    {
+        public void Quit()
+        {
+            GameManager.Quit();
+        }
+    }
+}
