2775650354281af0adb77eabfbda5ac21a69f249
Kfir Nissim <kfirr99@gmail.com>
2020-02-01 04:20:20 +0200

    add more trees

---
+++ Assets/GGJ2020/Prefabs/UI/QuitButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GGJ2020
+{
+    public class QuitButton : MonoBehaviour
+    {
+        public void Quit()
+        {
+            GameManager.Quit();
+        }
+    }
+}
